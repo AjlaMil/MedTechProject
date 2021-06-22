95941a54f88db3a19511cb169948b22334eac53a
Ajla <ajla@procerasoft.com>
2021-06-22 21:10:48 +0200

    createUser, getUser, getUsers

--- backend/MedTech1/functions/getUsers.js
+++ backend/MedTech1/functions/getUsers.js
@@ -1,18 +1,15 @@
 "use strict"
 
 const http = require("../utils/http")
+
 const { connect } = require("../db/connection")
 const  User  = require("../db/userSchema")
 
 module.exports.handler = async (event) => {
 
-    const { id } = event.pathParameters;
-    console.log(id)
     await connect();
 
-    const user = await User.findOne({ _id: id});
-    console.log(user)
-
-
-    return http.ok({ user })
+    const users = await User.find();
+    
+    return http.ok({ users })
 } 
